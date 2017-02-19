78fab7a10fed9f0b348113ffc7bafc953ce49d48
pignickyip <nickyipuk@gmail.com>
2017-02-19 19:03:01 +0800

    HeHeXposed Backup  Seems good I hate this project.................................Hop te god bless me
    Future: Mode Change function
    
    If anyone want to help me, send an email to me......15049782d@connect.polyu.hk
    
    Hope the God Save Me.... AHAHAAAHAH
    
    Hope the joupsuccess:(

--- app/src/main/java/com/hehe/hehexposedlocation/mode/ModeActivity.java
+++ app/src/main/java/com/hehe/hehexposedlocation/mode/ModeActivity.java
@@ -25,6 +25,8 @@ import com.hehe.hehexposedlocation.Common;
 import com.hehe.hehexposedlocation.R;
 import com.hehe.hehexposedlocation.feedback.FeedbackActivity;
 
+import org.w3c.dom.Text;
+
 import java.util.Calendar;
 
 public class ModeActivity extends Activity {
@@ -33,7 +35,12 @@ public class ModeActivity extends Activity {
     private SharedPreferences.Editor PE = null;
 
     private TextView restmode = null;
+    private TextView RestStartTime = null;
+    private TextView RestEndTime = null;
+
     private TextView workmode = null;
+    private TextView WorkStartTime = null;
+    private TextView WorkEndTime = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,61 +51,151 @@ public class ModeActivity extends Activity {
         clearsSetting();
         ToggleSetting();
     }
-    private void initSet(){
+    private void initSet() {
         Work = getSharedPreferences(Common.MODE_WORK_SETUP, MODE_WORLD_READABLE);
+
         workmode = (TextView) findViewById(R.id.workmode);
-        Boolean Work_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_KEY,false);
-        Boolean WorkStart_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_STARTTIME_KEY,false);
-        Boolean WorkEnd_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_ENDTIME_KEY,false);
-        if( Work_Setup && WorkStart_Setup && WorkEnd_Setup) {
+        WorkStartTime = (TextView) findViewById(R.id.workmode_starttime);
+        WorkEndTime = (TextView) findViewById(R.id.workmode_endtime);
+
+        Boolean Work_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_KEY, false);
+        Boolean WorkStart_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_STARTTIME_KEY, false);
+        Boolean WorkEnd_Setup = Work.getBoolean(Common.MODE_WORK_SETUP_ENDTIME_KEY, false);
+        int WorkStartTimeHourValue = Work.getInt(Common.MODE_WORK_SETUP_STARTTIME_KEY_HOUR, 0);
+        int WorkStartTimeMinValue = Work.getInt(Common.MODE_WORK_SETUP_STARTTIME_KEY_MINUTES, 0);
+        int WorkEndTimeHourValue = Work.getInt(Common.MODE_WORK_SETUP_ENDTIME_KEY_HOUR, 0);
+        int WorkEndTimeMinValue = Work.getInt(Common.MODE_WORK_SETUP_ENDTIME_KEY_MINUTES, 0);
+        if (Work_Setup && WorkStart_Setup && WorkEnd_Setup) {
             String msg = "Work mode is working";
             workmode.setText(msg);
-        }
-        else if(Work_Setup || !(WorkStart_Setup && WorkEnd_Setup)){
-            if(Work_Setup) {
+            msg = "Your Work mode time will Start from " + WorkStartTimeHourValue + ":" + WorkStartTimeMinValue + ".";
+            WorkStartTime.setText(msg);
+            msg = "Your Work mode time will End from " + WorkEndTimeHourValue + ":" + WorkEndTimeMinValue + ".";
+            WorkEndTime.setText(msg);
+        } else if (Work_Setup || !(WorkStart_Setup && WorkEnd_Setup)) {
+            if (Work_Setup) {
                 String msg = "Work mode is not working since not yet set up the start time and end time";
                 workmode.setText(msg);
-            }
-            else if(WorkStart_Setup || WorkEnd_Setup){
+                if (WorkStart_Setup) {//HAVE START TIME
+                    msg = "Your Work mode time will Start from " + WorkStartTimeHourValue + ":" + WorkStartTimeMinValue + ".";
+                    WorkStartTime.setText(msg);
+                    msg = "Have not set up the Work mode End Time.";
+                    WorkEndTime.setText(msg);
+                } else {
+                    msg = "Have not set up the Work mode Start Time.";
+                    WorkStartTime.setText(msg);
+                    msg = "Your Work mode time will End from " + WorkEndTimeHourValue + ":" + WorkEndTimeMinValue + ".";
+                    WorkEndTime.setText(msg);
+                }
+            } else if (WorkStart_Setup || WorkEnd_Setup) {
                 String msg = "Work mode is not working since only one time settle up";
                 workmode.setText(msg);
-            }
-            else {
+                if (WorkStart_Setup) {//HAVE START TIME
+                    msg = "Your Work mode time will Start from " + WorkStartTimeHourValue + ":" + WorkStartTimeMinValue + ".";
+                    WorkStartTime.setText(msg);
+                    msg = "Have not set up the Work mode End Time.";
+                    WorkEndTime.setText(msg);
+                } else {
+                    msg = "Have not set up the Work mode Start Time.";
+                    WorkStartTime.setText(msg);
+                    msg = "Your Work mode time will End from " + WorkEndTimeHourValue + ":" + WorkEndTimeMinValue + ".";
+                    WorkEndTime.setText(msg);
+                }
+            } else {
                 String msg = "Work mode is not working";
                 workmode.setText(msg);
+                msg = "Have not set up the Work mode Start Time.";
+                WorkStartTime.setText(msg);
+                msg = "Have not set up the Work mode End Time.";
+                WorkEndTime.setText(msg);
             }
-        }
-        else {
+        } else if (WorkStart_Setup && WorkEnd_Setup) {
+            String msg = "Work mode is ready for you enable it.";
+            workmode.setText(msg);
+            msg = "Your Work mode time will Start from " + WorkStartTimeHourValue + ":" + WorkStartTimeMinValue + ".";
+            WorkStartTime.setText(msg);
+            msg = "Your Work mode time will End from " + WorkEndTimeHourValue + ":" + WorkEndTimeMinValue + ".";
+            WorkEndTime.setText(msg);
+        } else {
             String msg = "Work mode is not working";
             workmode.setText(msg);
+            msg = "Have not set up the Work mode Start Time.";
+            WorkStartTime.setText(msg);
+            msg = "Have not set up the Work mode End Time.";
+            WorkEndTime.setText(msg);
         }
 
         Rest = getSharedPreferences(Common.MODE_REST_SETUP, MODE_WORLD_READABLE);
         restmode = (TextView) findViewById(R.id.restmode);
-        Boolean Rest_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_KEY,false);
-        Boolean RestStart_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_STARTTIME_KEY,false);
-        Boolean RestEnd_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_ENDTIME_KEY,false);
-        if(Rest_Setup && RestStart_Setup && RestEnd_Setup) {
+        RestStartTime = (TextView) findViewById(R.id.restmode_starttime);
+        RestEndTime = (TextView) findViewById(R.id.restmode_endtime);
+
+        Boolean Rest_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_KEY, false);
+        Boolean RestStart_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_STARTTIME_KEY, false);
+        Boolean RestEnd_Setup = Rest.getBoolean(Common.MODE_REST_SETUP_ENDTIME_KEY, false);
+
+        int RestStartTimeHourValue = Rest.getInt(Common.MODE_REST_SETUP_STARTTIME_KEY_HOUR, 0);
+        int RestStartTimeMinValue = Rest.getInt(Common.MODE_REST_SETUP_STARTTIME_KEY_MINUTES, 0);
+        int RestEndTimeHourValue = Rest.getInt(Common.MODE_REST_SETUP_ENDTIME_KEY_HOUR, 0);
+        int RestEndTimeMinValue = Rest.getInt(Common.MODE_REST_SETUP_ENDTIME_KEY_MINUTES, 0);
+        if (Rest_Setup && RestStart_Setup && RestEnd_Setup) {
             String msg = "Rest mode is working";
             restmode.setText(msg);
-        }
-        else if(Rest_Setup || !(RestStart_Setup && RestEnd_Setup)){
-            if(Rest_Setup) {
+            msg = "Your Rest mode time will Start from " + RestStartTimeHourValue + ":" + RestStartTimeMinValue + ".";
+            RestStartTime.setText(msg);
+            msg = "Your Rest mode time will End from " + RestEndTimeHourValue + ":" + RestEndTimeMinValue + ".";
+            RestEndTime.setText(msg);
+        } else if (Rest_Setup || !(RestStart_Setup && RestEnd_Setup)) {
+            if (Rest_Setup) {
                 String msg = "Rest mode is not working since not yet set up the start time and end time";
                 restmode.setText(msg);
-            }
-            else if(RestStart_Setup || RestEnd_Setup){
-                String msg = "Rest mode is not working since not yet set up the start time and end time";
+                if (RestStart_Setup) {//HAVE START TIME
+                    msg = "Your Rest mode time will Start from " + RestStartTimeHourValue + ":" + RestStartTimeMinValue + ".";
+                    RestStartTime.setText(msg);
+                    msg = "Have not set up the Rest mode End Time.";
+                    RestEndTime.setText(msg);
+                } else {
+                    msg = "Have not set up the Rest mode Start Time.";
+                    RestStartTime.setText(msg);
+                    msg = "Your Rest mode time will End from " + RestEndTimeHourValue + ":" + RestEndTimeMinValue + ".";
+                    RestEndTime.setText(msg);
+                }
+            } else if (RestStart_Setup || RestEnd_Setup) {
+                String msg = "Rest mode is not working since only one time settle up";
                 restmode.setText(msg);
-            }
-            else {
+                if (RestStart_Setup) {//HAVE START TIME
+                    msg = "Your Rest mode time will Start from " + RestStartTimeHourValue + ":" + RestStartTimeMinValue + ".";
+                    RestStartTime.setText(msg);
+                    msg = "Have not set up the Rest mode End Time.";
+                    RestEndTime.setText(msg);
+                } else {
+                    msg = "Have not set up the Rest mode Start Time.";
+                    RestStartTime.setText(msg);
+                    msg = "Your Rest mode time will End from " + RestEndTimeHourValue + ":" + RestEndTimeMinValue + ".";
+                    RestEndTime.setText(msg);
+                }
+            } else {
                 String msg = "Rest mode is not working";
                 restmode.setText(msg);
+                msg = "Have not set up the Rest mode Start Time.";
+                RestStartTime.setText(msg);
+                msg = "Have not set up the Rest mode End Time.";
+                RestEndTime.setText(msg);
             }
-        }
-        else {
+        } else if (RestStart_Setup && RestEnd_Setup) {
+            String msg = "Rest mode is ready for you enable it.";
+            restmode.setText(msg);
+            msg = "Your Rest mode time will Start from " + RestStartTimeHourValue + ":" + RestStartTimeMinValue + ".";
+            RestStartTime.setText(msg);
+            msg = "Your Rest mode time will End from " + RestEndTimeHourValue + ":" + RestEndTimeMinValue + ".";
+            RestEndTime.setText(msg);
+        } else {
             String msg = "Rest mode is not working";
             restmode.setText(msg);
+            msg = "Have not set up the Rest mode Start Time.";
+            RestStartTime.setText(msg);
+            msg = "Have not set up the Rest mode End Time.";
+            RestEndTime.setText(msg);
         }
     }
     //TODO overlap checking
@@ -122,6 +219,7 @@ public class ModeActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         restmode.setText( "The Start time set as " + selectedHour + ":" + selectedMinute);
+                        RestStartTime.setText("Current Start time set as " + selectedHour + ":" + selectedMinute);
                         PE = Rest.edit();
                         PE.remove(Common.MODE_REST_SETUP_STARTTIME_KEY);
                         PE.remove(Common.MODE_REST_SETUP_STARTTIME_KEY_HOUR);
@@ -150,6 +248,7 @@ public class ModeActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         restmode.setText( "The End time set as " + selectedHour + ":" + selectedMinute);
+                        RestEndTime.setText("Current End time set as " + selectedHour + ":" + selectedMinute);
                         PE = Rest.edit();
                         PE.remove(Common.MODE_REST_SETUP_ENDTIME_KEY);
                         PE.remove(Common.MODE_REST_SETUP_ENDTIME_KEY_HOUR);
@@ -178,6 +277,8 @@ public class ModeActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         workmode.setText( "The Start time set as " + selectedHour + ":" + selectedMinute);
+                        WorkStartTime.setText("Current Start time set as " + selectedHour + ":" + selectedMinute);
+
                         PE = Work.edit();
                         PE.remove(Common.MODE_WORK_SETUP_STARTTIME_KEY);
                         PE.remove(Common.MODE_WORK_SETUP_STARTTIME_KEY_HOUR);
@@ -205,6 +306,8 @@ public class ModeActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         workmode.setText( "The End time set as " + selectedHour + ":" + selectedMinute);
+                        WorkEndTime.setText("Current End time set as " + selectedHour + ":" + selectedMinute);
+
                         PE = Work.edit();
                         PE.remove(Common.MODE_WORK_SETUP_ENDTIME_KEY);
                         PE.remove(Common.MODE_WORK_SETUP_ENDTIME_KEY_HOUR);
